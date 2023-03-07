07c8d0eada90dc4315e00f7f3a65150fb3972b2c
Ján Popeláš <janopopelas@gmail.com>
2023-03-07 18:51:27 +0100

    refactor: extract methods

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -6,6 +6,27 @@ namespace TheatricalPlayersRefactoringKata
 {
     public class StatementPrinter
     {
+        private static int GetTragedyPlay(int audience)
+        {
+            if (audience < 30)
+            {
+                return 40000;
+            }
+
+            return 40000 + 1000 * (audience - 30);
+        }
+
+        private static int GetComedyPlay(int audience)
+        {
+            int result = 30000 + 300 * audience;
+            if (audience > 20)
+            {
+                result += 10000 + 500 * (audience - 20);
+            }
+
+            return result;
+        }
+
         public static string Print(Invoice invoice, Dictionary<string, Play> plays)
         {
             var totalAmount = 0;
@@ -13,37 +34,34 @@ namespace TheatricalPlayersRefactoringKata
             var result = $"Statement for {invoice.Customer}\n";
             var cultureInfo = new CultureInfo("en-US");
 
-            foreach(var perf in invoice.Performances) 
+            foreach (var performance in invoice.Performances)
             {
-                var play = plays[perf.PlayId];
-                int thisAmount;
-                switch (play.Type) 
+                if (performance.Audience > 30)
+                {
+                    volumeCredits += performance.Audience - 30;
+                }
+
+                var play = plays[performance.PlayId];
+                int playAmount;
+                switch (play.Type)
                 {
                     case "tragedy":
-                        thisAmount = 40000;
-                        if (perf.Audience > 30) {
-                            thisAmount += 1000 * (perf.Audience - 30);
-                        }
+                        playAmount = GetTragedyPlay(performance.Audience);
                         break;
                     case "comedy":
-                        thisAmount = 30000;
-                        if (perf.Audience > 20) {
-                            thisAmount += 10000 + 500 * (perf.Audience - 20);
-                        }
-                        thisAmount += 300 * perf.Audience;
+                        playAmount = GetComedyPlay(performance.Audience);
+                        volumeCredits += (int) Math.Floor((decimal) performance.Audience / 5);
                         break;
                     default:
                         throw new Exception("unknown type: " + play.Type);
                 }
-                // add volume credits
-                volumeCredits += Math.Max(perf.Audience - 30, 0);
-                // add extra credit for every ten comedy attendees
-                if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
 
                 // print line for this order
-                result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
-                totalAmount += thisAmount;
+                result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name,
+                    Convert.ToDecimal(playAmount / 100), performance.Audience);
+                totalAmount += playAmount;
             }
+
             result += string.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
             result += $"You earned {volumeCredits} credits\n";
             return result;
